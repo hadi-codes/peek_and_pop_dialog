cbe3fcbf480eb162d73e67ac968c96860b8aeef6
bigblackclock <abdlhadi1999apdo@gmail.com>
2020-09-28 18:43:30 +0200

    changed bloc to flutter/bloc

--- lib/cubit/peek_and_pop_dialog_cubit.dart
+++ lib/cubit/peek_and_pop_dialog_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'peek_and_pop_dialog_state.dart';
 
--- lib/peek_and_pop_dialog.dart
+++ lib/peek_and_pop_dialog.dart
@@ -1,6 +1,5 @@
 library peek_and_pop_dialog;
 
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
